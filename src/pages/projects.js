9c053c3ffe3d8dd97246596330060c8b2820e77c
Omar Farid <omarfarid222@gmail.com>
2024-02-07 15:02:38 +0200

    Optimizing Next.js Image Component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -19,6 +19,8 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
         <FramerImage src={img} alt={title} className='w-full h-auto'
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
         />
       </Link>
       <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
